590225d5df8456cf5e28e040a3f419367c942b34
tasiorbr <46011179+tasiorbr@users.noreply.github.com>
2019-02-01 19:01:58 -0500

    Updates for adding encoder

---
+++ src/main/java/frc/robot/commands/ManualDriveCartesian.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class ManualDriveCartesian extends Command {
+  public ManualDriveCartesian() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.mecDriveSubsystem);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double moveX= 0.5*Robot.oi.rightstick.getRawAxis(0);
+    double moveY = 0.5*-Robot.oi.leftstick.getRawAxis(1);
+    double rotateZ = 0.5*Robot.oi.rightstick.getRawAxis(2);
+
+    Robot.mecDriveSubsystem.driveC(moveX, moveY, rotateZ);
+    
+  
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+    
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/MecDriveSubsystem.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.ManualDriveCartesian;
+
+/**
+ * Add your docs here.
+ */
+public class MecDriveSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  // instantiate new motor controller objects
+  
+  public VictorSP driveFrontLeft = new VictorSP(RobotMap.motorPortFrontLeft);
+  public VictorSP driveFrontRight = new VictorSP(RobotMap.motorPortFrontRight);
+  public VictorSP driveRearLeft = new VictorSP(RobotMap.motorPortRearLeft);
+  public VictorSP driveRearRight = new VictorSP(RobotMap.motorPortRearRight);
+
+  // instantiate a new MecanumDrive object and assign motor controllers to it
+  // Note:  If motor controllers need to be inverted, do that first (or as part of the the joystick call)
+  public MecanumDrive mDrive = new MecanumDrive(driveFrontLeft, driveRearLeft, driveFrontRight, driveRearRight);
+     
+    public MecDriveSubsystem() {
+       mDrive.setSafetyEnabled(false);
+       // mDrive.setExpiration(0.1);
+        mDrive.setMaxOutput(1.0);
+       // mDrive.setDeadband(0.05);
+    }
+
+
+   // add manualDrive() method  
+   // public void manualDrive(double move, double turn) {
+   //   drive.arcadeDrive(move, turn);
+   // }
+   // public void driveCartesian​(double ySpeed, double xSpeed, double zRotation)
+    public void driveC(double moveX, double moveY, double rotateZ) {
+      mDrive.driveCartesian(moveX, moveY, rotateZ);   
+     }
+ 
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new ManualDriveCartesian());
+  }
+}
